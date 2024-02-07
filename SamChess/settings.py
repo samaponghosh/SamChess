dfa412e56046535d2cdcd51a252edc958fc2720d
samaponghosh <samaponghosh2002@gmail.com>
2024-02-07 12:32:42 +0530

    Fourteenth Commit

--- SamChess/settings.py
+++ SamChess/settings.py
@@ -39,7 +39,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    # 'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'SamChess.urls'
