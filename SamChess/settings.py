52ec7090c38c3ac96abaf93caca7fec3e099db1d
samaponghosh <samaponghosh2002@gmail.com>
2024-02-07 12:11:38 +0530

    Twelveth Commit

--- SamChess/settings.py
+++ SamChess/settings.py
@@ -101,10 +101,10 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'] = dj_database_url.config(default='postgres://')
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# # DATABASES['default'] = dj_database_url.config(default='postgres://')
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
