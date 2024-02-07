04bb34be51d7a6af1df48a8e6e79b1a267ae0246
samaponghosh <samaponghosh2002@gmail.com>
2024-02-07 11:30:39 +0530

    Seventh commit

--- SamChess/settings.py
+++ SamChess/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-week)+)h(jir5i2^ca=q^z807b)ew#f^pq%ubzbwu_ndgsaz)l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -144,10 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS=[
-#     # os.path.join(BASE_DIR, "static_cdn"),
-# ]
+STATICFILES_DIRS= os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
