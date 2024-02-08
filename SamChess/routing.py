b448440ade6c557eb9935c98d5f23129482f312d
samaponghosh <samaponghosh2002@gmail.com>
2024-02-08 15:51:04 +0530

    Fullfinal commit

--- SamChess/routing.py
+++ SamChess/routing.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-# from channels.http import AsgiHandler
+from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
