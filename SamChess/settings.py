d99b10757ff2018c111babb8d5b904b29d908dd8
samaponghosh <samaponghosh2002@gmail.com>
2024-02-09 12:43:41 +0530

    second commit

--- SamChess/settings.py
+++ SamChess/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "1a8b5d8922cd4ca4bf3ac4c5887bd3fe"
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = "1a8b5d8922cd4ca4bf3ac4c5887bd3fe"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG","True")
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","True")
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
