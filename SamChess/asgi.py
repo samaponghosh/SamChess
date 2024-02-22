13b0d821c12408535e6d2c72e68346f1fe0fae56
samaponghosh <samaponghosh2002@gmail.com>
2024-02-23 00:48:32 +0530

    render11 commit

--- SamChess/asgi.py
+++ SamChess/asgi.py
@@ -3,6 +3,8 @@
 import os
 import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SamChess.settings')
+django.setup()
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
@@ -12,8 +14,6 @@ from channels.auth import AuthMiddlewareStack
 
 from ChessApp.consumers import GameConsumer, SingleConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SamChess.settings')
-django.setup()
 
 # application = get_asgi_application()
 application = ProtocolTypeRouter({
